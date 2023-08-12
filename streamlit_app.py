607386f27753d167d5284b84626eab59193a4cba
Kartikey Mishra <76617485+KartikeyMish@users.noreply.github.com>
2023-08-13 02:21:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ with st.sidebar:
         st.success('API key already provided!', icon='✅')
         openai.api_key = st.secrets['OPENAI_API_KEY']
     else:
-        st.caption('Find your API key [here](https://beta.openai.com/account/api-keys)', icon='🔑')
+        st.caption(':key: Find your API key [here](https://beta.openai.com/account/api-keys)')
         openai.api_key = st.text_input('Enter OpenAI API key:', type='password')
         if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')           

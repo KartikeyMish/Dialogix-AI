a4d2b5241525aca326887c2c061fa0aeeeb6efcd
Kartikey Mishra <76617485+KartikeyMish@users.noreply.github.com>
2023-08-13 01:59:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Dialogbox-AI",
-    page_icon="🧠",
-    layout="wide",
-    initial_sidebar_state="auto",
-    menu_items={
-        'Code': 'https://github.com/kartikeymishra/Dialogbox-AI',
-        'Connect': "https://www.linkedin.com/in/kartikeymish/"
-        # 'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    page_icon="🧠"
 )
 
 st.title('🧠 Dialogbox-AI 🗣️')
